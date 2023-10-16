4edd3e5a15ae70d5e3b1cdd017bbe4c7a3de444f
CPRToken <99814986+CPRToken@users.noreply.github.com>
2023-10-17 00:00:06 +0700

    fixed errors

--- src/pages/api/cronTrigger.ts
+++ src/pages/api/cronTrigger.ts
@@ -1,3 +1,4 @@
+import { NextApiRequest, NextApiResponse } from  'next/types';
 import { sendScheduledEmails } from './schedule';
 // Assuming schedule.ts is in the same directory
 import cron from 'node-cron';
@@ -5,6 +6,6 @@ import cron from 'node-cron';
 // Schedule the function to run every minute
 cron.schedule('* * * * *', sendScheduledEmails);
 
-export default (req, res) => {
+export default (req: NextApiRequest, res: NextApiResponse) => {
   res.status(200).send('Cron job is running.');
 };
